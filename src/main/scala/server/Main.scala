bf3c5839fcdbee6d43dcc76a7e1d7a8ceb9253a7
WangTaoTheTonic <wangtao111@huawei.com>
2015-02-03 20:45:55 +0800

    handle the passed in config file

--- src/main/scala/server/Main.scala
+++ src/main/scala/server/Main.scala
@@ -1,5 +1,7 @@
 package server
 
+import _root_.scala.Predef._
+import _root_.scala.sys
 import akka.event.Logging
 import server.domain.actors.{Supervisor, JobActor, ContextManagerActor}
 
@@ -10,6 +12,7 @@ import akka.pattern.ask
 import akka.actor.{Props, ActorSystem}
 import com.typesafe.config.ConfigFactory
 import server.domain.actors.timeout
+import java.io.File
 
 /**
  * Created by raduc on 29/10/14.
@@ -18,7 +21,17 @@ object Main {
   def main(args: Array[String]) {
 
     val defaultConfig = ConfigFactory.load()
-    val masterConfig = defaultConfig.getConfig("manager")
+    val config = if (args.length > 0) {
+      val configFile = new File(args(0))
+      if (!configFile.exists()) {
+        println("Could not find configuration file " + configFile)
+        sys.exit(1)
+      }
+      ConfigFactory.parseFile(configFile).withFallback(defaultConfig)
+    } else {
+      defaultConfig
+    }
+    val masterConfig = config.getConfig("manager")
     val system = ActorSystem("ManagerSystem", masterConfig)
 
     val supervisor = system.actorOf(Props(classOf[Supervisor]), "Supervisor")
